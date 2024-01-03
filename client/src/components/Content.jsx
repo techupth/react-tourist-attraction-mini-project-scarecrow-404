0d638ea64b069d093d938428df6371a7555f323a
Kittipoom Puttaruksa <kittipoom@Kittipooms-MacBook-Pro.local>
2024-01-03 22:06:04 +0700

    style:add simple styling and font

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -36,88 +36,91 @@ function Content() {
     navigator.clipboard.writeText(url);
   }
   return (
-    <div className="flex flex-col justify-center w-[100%] gap-7">
-      <input
-        type="text"
-        placeholder="หาที่เที่ยวแล้วไปกัน..."
-        className=" border-b-2 outline-none"
-        value={searchInput}
-        onChange={(e) => {
-          setSearchInput(e.target.value);
-        }}
-      />
+    <>
+      <h1 className=" pl-16">ค้นหาที่เที่ยว</h1>
+      <div className="flex flex-col items-center w-[100%] gap-7 p-10 ">
+        <input
+          type="text"
+          placeholder="หาที่เที่ยวแล้วไปกัน..."
+          className=" border-b-2 outline-none text-center w-[85%]"
+          value={searchInput}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+        />
 
-      {status === "success" ? (
-        data.map((item, index) => {
-          return (
-            <div key={index} className="flex flex-row gap-4">
-              <img
-                src={item.photos[0]}
-                alt=""
-                className="w-[150px] h-[170px] rounded-xl"
-              />
-              <div className="flex flex-col gap-2">
-                <a href={item.url} target="_blank">
-                  {item.title}
-                </a>
-                <p>
-                  {item.description.substring(0, 100)}...
-                  <a
-                    className=" text-blue-400 underline"
-                    href={item.url}
-                    target="_blank"
-                  >
-                    อ่านต่อ
+        {status === "success" ? (
+          data.map((item, index) => {
+            return (
+              <div key={index} className="flex flex-row gap-4 w-[80%]">
+                <img
+                  src={item.photos[0]}
+                  alt=""
+                  className="w-[250px] h-[200px] rounded-xl"
+                />
+                <div className="flex flex-col gap-2">
+                  <a href={item.url} target="_blank" className=" text-xl">
+                    {item.title}
                   </a>
-                </p>
-                <p>
-                  หมวด:
-                  {item.tags.map((tag, index) => {
-                    return (
-                      <button
-                        key={index}
-                        className=" ml-1"
-                        onClick={() => {
-                          handleCategoryClick(tag);
-                        }}
-                      >
-                        {tag}
-                      </button>
-                    );
-                  })}
-                </p>
-                <div className="flex gap-5">
-                  {item.photos.map((photo, index) => {
-                    return index > 0 ? (
-                      <img
-                        key={index}
-                        src={photo}
-                        alt=""
-                        className=" w-[75px] h-[75px] rounded-xl"
-                      />
-                    ) : (
-                      ""
-                    );
-                  })}
+                  <p className=" text-gray-600">
+                    {item.description.substring(0, 100)}...
+                    <a
+                      className=" text-blue-400 underline"
+                      href={item.url}
+                      target="_blank"
+                    >
+                      อ่านต่อ
+                    </a>
+                  </p>
+                  <p className="text-gray-6007">
+                    หมวด:
+                    {item.tags.map((tag, index) => {
+                      return (
+                        <button
+                          key={index}
+                          className=" ml-1 text-gray-600 underline"
+                          onClick={() => {
+                            handleCategoryClick(tag);
+                          }}
+                        >
+                          {tag}
+                        </button>
+                      );
+                    })}
+                  </p>
+                  <div className="flex gap-5">
+                    {item.photos.map((photo, index) => {
+                      return index > 0 ? (
+                        <img
+                          key={index}
+                          src={photo}
+                          alt=""
+                          className=" w-[75px] h-[75px] rounded-xl"
+                        />
+                      ) : (
+                        ""
+                      );
+                    })}
+                  </div>
                 </div>
+                <Tooltip title="Copy URL">
+                  <IconButton
+                    onClick={() => {
+                      copyUrlToClipboard(item.url);
+                    }}
+                    style={{ color: "blue" }}
+                  >
+                    <ShareIcon />
+                  </IconButton>
+                </Tooltip>
               </div>
-              <Tooltip title="Copy URL">
-                <IconButton
-                  onClick={() => {
-                    copyUrlToClipboard(item.url);
-                  }}
-                  style={{ color: "blue" }}
-                >
-                  <ShareIcon />
-                </IconButton>
-              </Tooltip>
-            </div>
-          );
-        })
-      ) : (
-        <h1>{status}</h1>
-      )}
-    </div>
+            );
+          })
+        ) : (
+          <h1>{status}</h1>
+        )}
+      </div>
+    </>
   );
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div>
-      <h1 className=" text-center text-blue-500 text-3xl">เที่ยวไหนดี</h1>
+      <h1 className=" text-center text-blue-500 text-4xl p-16">เที่ยวไหนดี</h1>
     </div>
   );
 }
